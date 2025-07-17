4cf32f13bb0c65caee5c7603f23082285b6567f7
Luiz Henrique Reis do Nascimento <kinhoreis2000@gmail.com>
2025-07-16 22:59:46 -0700

    Project dockerized and kubernetes with kind

--- src/server.js
+++ src/server.js
@@ -14,18 +14,21 @@ app.use(express.json())
 app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER))
 app.use(routes)
 
-app.use((error, req, res, next)=>{
-  if(error instanceof AppError){
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
     return res.status(error.statusCode).json({
-      status:'error',
-      message:error.message
+      status: 'error',
+      message: error.message
     })
-  } else{
-  return res.status(500).json({
-    status:'error',
-    message:'Internal server error'
-  })
   }
+
+  console.error('🔥 Erro inesperado:', error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error'
+  })
 })
-const PORT = process.env.SERVER_PORT || 3220
-app.listen(PORT, ()=> console.log(`Server is running on ${PORT}`))
+
+const PORT = process.env.SERVER_PORT || 5000
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
